58b460915803815ae6198844de0afb57d02d0530
Moritz Asche <moritz@asches.de>
2021-01-26 14:53:10 -0500

    Got the promt for the from number systems working

--- NumberSystemConverter/main.swift
+++ NumberSystemConverter/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+// Input
+
+//Get the "from" numbersystems
+print("What number system do you want to convert from?")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice (B/O/D/H): ", terminator: "") // keep following output on the same line
+let from = readLine()!
+
+
+// Process
+print("\(from)")
+
+
+//use switch statement and 
+
+
+
+//Output
 
