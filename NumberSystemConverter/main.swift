8d34471edceb8a7c4a205bf90179eb1dea8743b3
Moritz Asche <moritz@asches.de>
2021-01-26 22:12:45 -0500

    Doing some stuff

--- NumberSystemConverter/main.swift
+++ NumberSystemConverter/main.swift
@@ -16,31 +16,58 @@ print("O: Octal")
 print("D: Decimal")
 print("H: Hexadecimal")
 
-//find the value that we convert from
-print("Convert from (B/O/D/H): ", terminator: "") // keep following output on the same line
-let from = readLine()!
-//the value we convert to
-print("Convert to (B/O/D/H): ",terminator: "")
-let to = readLine()!
+////find the value that we convert from
+//print("Convert from (B/O/D/H): ", terminator: "") // keep following output on the same line
+
+
 //the number we have to convert
 print("Number to convert: ",terminator: "")
 let toConvert = readLine()!
 
+
+
 // Process
 
-func firstStep(from: Int, to: Int) -> Int {
-    <#function body#>
+print("Convert from (B/O/D/H): ", terminator: "") // keep following output on the same line
+let from = readLine()!
+if from == ("B") {
+    let base = 2.0
+    var exponent = 0.0
+    var decimalEquivalent = 0.0
+
+    //iterate over each character
+    //from right to left
+
+    for character in toConvert.reversed() {
+        
+        if let digit = Double(String(character)) {
+            
+            //add the current sum
+            decimalEquivalent += digit * pow(base, exponent)
+            exponent += 1
+        }
+    }
+    print("\(decimalEquivalent)")
+
+}else if from == ("O") {
+    let base2 = 8.0
+    var decimalEquivalent2 = 0.0
+    var exponent2 = 0.0
+
+    if toConvert.contains("9") || toConvert.contains("8") {
+        print("ERROR")
+    } else {
+        for character in value2.reversed() {
+            if let digit2 = Double(String(character)) {
+                decimalEquivalent2 += digit2 * pow(base2, exponent2)
+                exponent2 += 1
+            }
+        }
+    }
+    decimalEquivalent2
 }
 
-
-
-
-
-
-print("From \(from)")
-print("To \(to)")
-print("With \(toConvert)")
-
+    
 
 
 //Output
